d8066afb788ef779a2ef61f099b4b4352d383dfe
pupulstudy <47462483+pupulstudy@users.noreply.github.com>
2019-07-07 23:36:13 +0700

    Create exercise-10.js

---
+++ exercise-10.js
@@ -0,0 +1,8 @@
+const bandingkanAngka = (angka1, angka2) => angka1 > angka2 ? false : angka2 > angka1 ? true : -1;
+
+// TEST CASES
+console.log(bandingkanAngka(5, 8)); // true
+console.log(bandingkanAngka(5, 3)); // false
+console.log(bandingkanAngka(4, 4)); // -1
+console.log(bandingkanAngka(3, 3)); // -1
+console.log(bandingkanAngka(17, 2)); // false
