461fdcb94d3c807050823132ff0d87fe82a8e8c0
pupulstudy <47462483+pupulstudy@users.noreply.github.com>
2019-07-08 00:41:42 +0700

    Create exercise-13.js

---
+++ exercise-13.js
@@ -0,0 +1,15 @@
+const xo = (foo) => {
+	let xFoo = 0;
+	let oFoo = 0;
+	for(let j = 0; j < foo.length; j++) {
+		foo.charAt(j) === 'x' ? xFoo++ : oFoo++;
+	}
+		return (xFoo === oFoo ? true : false);
+};
+
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
