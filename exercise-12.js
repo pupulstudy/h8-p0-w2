0484627d4aeae9b87306d4eac5c9459ab417c5e3
pupulstudy <47462483+pupulstudy@users.noreply.github.com>
2019-07-07 23:57:58 +0700

    Create exercise-12.js

---
+++ exercise-12.js
@@ -0,0 +1,8 @@
+const konversiMenit = foo => (foo % 60) < 10 ? `${Math.floor(foo / 60)}:0${foo % 60}` : `${Math.floor(foo / 60)}:${foo % 60}`;
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
